994c533e5ef5bafc6c2b29f662d231dd13c80780
Yukti-09 <44090430+Yukti-09@users.noreply.github.com>
2020-07-03 23:44:56 +0530

    Create CompareTheTriplets.py

---
+++ Problem Solving/Algorithms/Warmup/CompareTheTriplets.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    x=0;y=0
+    for i in range(0,3):
+        if(a[i]>b[i]):
+            x+=1
+        elif(a[i]<b[i]):
+            y+=1
+    return(x,y) 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
