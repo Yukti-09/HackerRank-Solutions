a83afd523af17791eab0327f5f4170cfa1ff76f7
Yukti-09 <44090430+Yukti-09@users.noreply.github.com>
2020-07-05 09:23:44 +0530

    Create TheHurdleRace.py

---
+++ Problem Solving/Algorithms/Implementation/TheHurdleRace.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hurdleRace function below.
+def hurdleRace(k, height):
+    if max(height)-k>0:
+        print(max(height)-k)
+        return max(height)-k
+    else:
+        print("0")
+        return 0
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
