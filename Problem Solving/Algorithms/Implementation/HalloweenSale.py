2b537a26193c08850b229bd9425386a71d501475
Yukti-09 <44090430+Yukti-09@users.noreply.github.com>
2020-07-05 14:22:47 +0530

    Create HalloweenSale.py

---
+++ Problem Solving/Algorithms/Implementation/HalloweenSale.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the howManyGames function below.
+def howManyGames(p, d, m, s):
+    count=-1
+    while s>=0:
+        s-=p
+        count+=1
+        if p>d and p-d>m:
+            p-=d
+        else:
+            p=m
+    print(count) 
+    return count   
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    pdms = input().split()
+
+    p = int(pdms[0])
+
+    d = int(pdms[1])
+
+    m = int(pdms[2])
+
+    s = int(pdms[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
