88834cde84604cff6fc3d2542383f70bfe690978
Yukti Khosla <yuktikhosla@gmail.com>
2020-09-04 19:10:45 +0530

    Create Capitalize.py

---
+++ Python/Strings/Capitalize.py
@@ -0,0 +1,15 @@
+
+# Complete the solve function below.
+def solve(s):
+    st=""
+    st+=s[0].upper()
+    for i in range(1,len(s)):
+        if s[i-1]==" ":
+            x=s[i].upper()
+            st+=x
+        else:
+            st+=s[i]
+    return st
+    
+
+
