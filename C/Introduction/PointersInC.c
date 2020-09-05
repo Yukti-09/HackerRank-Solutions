17e2828e5faeec9b0d046274727bb923e6e72912
Yukti Khosla <yuktikhosla@gmail.com>
2020-09-05 12:34:48 +0530

    Create PointersInC.c

---
+++ C/Introduction/PointersInC.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void update(int *a,int *b) 
+{
+    int temp=*a+*b;
+    if(*a>*b)
+    *b=*a-*b;
+    else
+    *b=*b-*a;
+    *a=temp;
+    // Complete this function    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
